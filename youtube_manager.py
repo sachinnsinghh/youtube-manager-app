0f97c2eb6a06989d7120d37b5ecafdc039a7db18
sachinnsinghh <sachinsingh000094gmail.com>
2024-07-04 00:37:46 +0530

    first comomit

---
+++ youtube_manager.py
@@ -0,0 +1,89 @@
+import json
+import os
+def load_data():
+    try:
+        if not os.path.exists('youtube.txt'):
+            return []
+        with open('youtube.txt', 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+def save_data_helper(videos):
+    with open('youtube.txt', 'w') as file:
+        json.dump(videos , file)
+    
+def list_all_videos(videos):
+    print("/n")
+    print("*" *70)
+    for index, video in enumerate(videos, start =1):
+        
+        print(f"{index}. {video['name']}, Duration:{video['time']} ")
+    print("\n")
+    print("*" *70)
+
+def add_video(videos):
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name': name, "time": time})
+    save_data_helper(videos)
+
+def update_video(videos):
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update"))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name")
+        time = input("Enter the new video time")
+        videos[index-1] = {'name':name , "time":time}
+        save_data_helper(videos)
+    else:
+        print("invalid index selected")
+        
+        
+def delete_video(videos):
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted"))
+    
+    if 1 <= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("invalid video index selected ")
+
+
+
+
+
+def main():
+    videos = load_data()
+    while True:
+        print("\n Youtube Manager | choose and option ")
+        print("1.  List all youtube videos")
+        print("2.  Add youtube videos")
+        print("3.  Update youtube video Details")
+        print("4.  Delete youtube videos")
+        print("5.  Exit the app ")
+
+        choice = input("Enter Your Choice:")
+        print(videos)
+        match choice:
+            case '1':
+                list_all_videos(videos)
+                
+            case '2':
+                add_video(videos)
+
+            case '3':
+                update_video(videos)
+                
+            case '4':
+                delete_video(videos)
+                
+            case '5':
+                break
+            case _:
+                print("Invalid Choice")
+                
+if __name__ == "__main__":
+    main()
